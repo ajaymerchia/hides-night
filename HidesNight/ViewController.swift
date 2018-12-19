110e477c6ec0e30cce7d1d6de34e3ddc02dab887
Ajay Raj Merchia <ajaymerchia@berkeley.edu>
2018-12-19 13:21:18 -0800

    Initial Commit

---
+++ HidesNight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HidesNight
+//
+//  Created by Ajay Merchia on 12/19/18.
+//  Copyright © 2018 Ajay Merchia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
